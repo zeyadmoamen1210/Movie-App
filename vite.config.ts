c54b769d1bf5d276f867434a928ce68102d40c7d
zeyadMoamen <zeyad.moamen@hamza.solutions>
2022-06-19 13:14:44 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Movie-App/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
-  }
+  },
+  base: './'
 })
